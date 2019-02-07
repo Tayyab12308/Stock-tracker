5a34f66cb0c19dd81944df11595a9e02554ca873
Tayyab Iqbal <tayyab12308@gmail.com>
2019-02-07 19:09:11 +0000

    fix issue with error messages

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,20 +1,14 @@
 class StocksController < ApplicationController
   
   def search
-    if params[:stock].present?
-      @stock = Stock.new_from_lookup(params[:stock])
-      if @stock
-        respond_to do |format|
-          format.js { render partial: 'users/result' }
-        end
-      else
-        flash[:danger] = "You have entered an incorect symbol"
-        redirect_to my_portfolio_path
-      end
+    if params[:stock].blank?
+      flash.now[:danger] = "Stock field cannot be blank"
     else
-      flash[:danger] = "Stock field cannot be blank"
-      redirect_to my_portfolio_path
+      @stock = Stock.new_from_lookup(params[:stock])
+      flash.now[:danger] = "You have entered an incorect symbol" unless @stock
+    end
+    respond_to do |format|
+      format.js { render partial: 'users/result' }
     end
   end
-  
 end
