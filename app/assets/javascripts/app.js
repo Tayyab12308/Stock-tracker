eb5627172007ad18f688379c2eaeb6badd46cb3b
Tayyab Iqbal <tayyab12308@gmail.com>
2019-03-18 04:48:59 +0000

    added friends functionality to the angular app

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,26 +1,41 @@
 var app = angular.module('StockTrackerApp', [])
-                 .factory('stockService',['$http', function($http){
+                 .factory('stockService', ['$http', function($http){
                    var stockApi = {};
                    
                    stockApi.searchStocks = function(symbol){
                      return $http.get('/search_stocks.json?stock=' + symbol);
                    }
+                   
+                   stockApi.addStockToPortfolio = function(symbol){
+                     return $http.post('/user_stocks.json?stock=' + symbol);
+                   }
                    return stockApi;
                  }])
-                  .controller('stocksController', ['$scope', 'stockService', function($scope, stockService){
+                 .factory('friendService',['$http', function($http){
+                   var friendApi ={};
+                   friendApi.searchFriends = function(search_param){
+                     return $http.get('/search_friends.json?search_param=' + search_param);
+                   }
+                   friendApi.addFriend = function(id){
+                     return $http.post('/add_friend.json?friend=' + id);
+                   }
+                   return friendApi;
+                 }])
+                 .controller('stocksController', ['$scope', 'stockService', function($scope, stockService){
                     $scope.stock = {};
                     $scope.lookup = function() {
                     if($scope.ticker != undefined && $scope.ticker != '') {
                       stockService.searchStocks($scope.ticker)
                           .then(function(response){
                                   $scope.stock.error        = null;
+                                  $scope.stock.message      = null;
                                   $scope.stock.symbol       = response.data.ticker;
                                   $scope.stock.name         = response.data.name;
                                   $scope.stock.last_price   = response.data.last_price;
-                                  $scope.can_be_added = response.data.can_be_added;
+                                  $scope.stock.can_be_added = response.data.can_be_added;
                                 },
                                 function(response){
-                                  $scope.stock = {};
+                                  $scope.stock       = {};
                                   $scope.stock.error = response.data.response;
                                 }); 
                       
@@ -28,4 +43,59 @@ var app = angular.module('StockTrackerApp', [])
                       $scope.stock = {}
                     }
                   }
+                  
+                  $scope.add = function(){
+                    if($scope.stock != undefined && $scope.stock.symbol != ''){
+                      stockService.addStockToPortfolio($scope.stock.symbol)
+                        .then(function(response){
+                                $scope.stock.error   = null;
+                                $scope.stock.message = response.data.response;
+                                $scope.stock.name    = null;
+                                $scope.ticker        = null;
+                                $('#stock-list').load('my_portfolio.js');
+                              },
+                              function(response){
+                                $scope.stock       = {};
+                                $scope.stock.error = response.data.response;
+                              });
+                    } else {
+                      $scope.stock.error = "Stock cannot be added";
+                    }
+                  }
+                 }])
+                 .controller('friendsController', ['$scope', 'friendService', function($scope, friendService){
+                   $scope.friends = {};
+                   $scope.lookup = function(){
+                    if($scope.friend_search_param != undefined && $scope.friend_search_param != ''){
+                      friendService.searchFriends($scope.friend_search_param)
+                        .then(function(response){
+                                $scope.friends.error   = null;
+                                $scope.friends.message = null;
+                                $scope.friends.list    = response.data;
+                              },
+                              function(response){
+                                $scope.friends       = {};
+                                $scope.friends.error = response.data.response;
+                              });
+                    } else {
+                      $scope.friends = {};
+                    }
+                    $scope.add = function(friend_id){
+                      $scope.friends = {};
+                      if(friend_id != undefined && friend_id != ''){
+                        friendService.addFriend(friend_id)
+                          .then(function(response){
+                                  $scope.friends.error       = null;
+                                  $scope.friends.message     = response.data.response;
+                                  $scope.friend_search_param = null;
+                                  $('#friends-list').load('my_friends.js');
+                                },
+                                function(response){
+                                  $scope.friends.error = response.data.response;
+                                })
+                      } else {
+                        $scope.friend.error = "Friend Cannot Be Added";
+                      }
+                    }
+                   }
                  }])
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,23 @@
 class UsersController < ApplicationController
+  protect_from_forgery with: :exception, unless: -> { request.format.json? }
 
   def my_portfolio
     @user = current_user
     @user_stocks = current_user.stocks
+    
+    respond_to do |format|
+      format.html { render :my_portfolio }
+      format.js   { render partial: 'stocks/list.html' }
+    end
   end
   
   def my_friends
     @friendships = current_user.friends
+    
+    respond_to do |format|
+      format.html {render :my_friends}
+      format.js {render partial: 'friends/list.html'}
+    end
   end
   
   def search
@@ -17,8 +28,15 @@ class UsersController < ApplicationController
       @users = current_user.except_current_user(@users)
       flash.now[:danger] = "No users match the search" if @users.blank?
     end
-    respond_to do |format|
-      format.js { render partial: 'friends/result' }
+    if @users.blank?
+      render status: 404, json: { response: "No users match the search criteria" }
+    else
+      @users.map do |user|
+        user.profile_path = user_path(user)
+        user.friends_already = current_user.friends_with?(user.id)
+        user
+      end
+      render json: @users, methods: [:profile_path, :friends_already]
     end
   end
   
@@ -26,11 +44,10 @@ class UsersController < ApplicationController
     @friend = User.find(params[:friend])
     current_user.friendships.build(friend_id: @friend.id)
     if current_user.save
-      flash[:success] = "Friend was successfully added"
+      render json: { response: flash[:success]  = "Friend was successfully added" }, status: :ok
     else
-      flash[:danger] = "There was something wrong with the friend request"
+      render json: {response: flash[:danger]= "There was something wrong with the friend request" }, status: 422
     end
-    redirect_to my_friends_path
   end
   
   def show
